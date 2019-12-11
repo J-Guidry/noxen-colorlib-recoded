6ae553c41c8f28033f175ae04d236a8d9608f87d
Guidry <jh_guidry@outlook.com>
2019-12-10 22:43:45 -0600

    projects text on hover implements on projects page, slider started, beginning of slider functinality in process of styling

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ const mobileBtn = document.querySelector(".mobile-btn");
 const exitMobileNav = document.querySelector(".cross");
 const mobileNav = document.querySelector(".mobile-nav");
 
+//TOGGLE MOBILE MENU
 mobileBtn.addEventListener("click", function(){
     mobileNav.classList.toggle("toggle-mobile");
 });
@@ -11,3 +12,4 @@ exitMobileNav.addEventListener("click", function(){
     mobileNav.classList.toggle("toggle-mobile");
 });
 
+// IMAGE SLIDESHOW
